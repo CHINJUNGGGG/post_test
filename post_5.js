7b104f5dfa46543a2ae0f541d40359cbeb760bf9
CHINJUNG <59245951+CHINJUNGGGG@users.noreply.github.com>
2021-08-25 01:39:15 +0700

    Create post_5.js

---
+++ post_5.js
@@ -0,0 +1,29 @@
+<textarea id="valueText"></textarea>
+<br>
+<button type="button" onclick="checkText()">
+Click
+</button>
+
+
+
+
+
+
+
+<script>
+function checkText(){
+	
+  var textGive = document.getElementById("valueText").value;  
+  
+  if (textGive.match(new RegExp("^[a-z]+$"))) {
+      resultado = "All Small Letter";
+  } else if (textGive.match(new RegExp("^[A-Z]+$"))) {
+      resultado = "All Capital Letter";
+  } else if (textGive.match(new RegExp("^[a-zA-Z]+$"))) {
+      resultado = "Mix";
+  }
+  
+	console.log(resultado)
+  
+}
+</script>
